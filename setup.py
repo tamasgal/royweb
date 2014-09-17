27100500b5052f8c90f095371dffa082a4c98bf5
tamasgal <himself@tamasgal.com>
2014-09-17 09:55:16 +0200

    Changes version number to 0.3.5 and adds new screenshot

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='royweb',
-      version='0.3.4',
+      version='0.3.5',
       url='http://github.com/tamasgal/royweb/',
       description='Restless Oyster online monitoring tool',
       author='Tamas Gal',
