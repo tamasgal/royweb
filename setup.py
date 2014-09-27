6474f4bc016ae697bdb5756976ce9964bf4d06d1
tamasgal <himself@tamasgal.com>
2014-09-27 13:21:05 +0200

    Changes version to 0.6.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='royweb',
-      version='0.6.2',
+      version='0.6.3',
       url='http://github.com/tamasgal/royweb/',
       description='Restless Oyster online monitoring tool',
       author='Tamas Gal',
