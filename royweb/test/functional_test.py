4bb22710a36a9e03c7fd0b280d30560cc54e2647
Tamas Gal <himself@tamasgal.com>
2014-11-10 16:38:43 +0100

    First function tests

---
+++ royweb/test/functional_test.py
@@ -0,0 +1,25 @@
+import unittest
+from selenium import webdriver
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_title(self):
+        # Edith heard about ROyWeb and fires up a browser to have a look at it
+        self.browser.get('http://localhost:8080')
+
+        # She notices the title of the page
+        self.assertIn('Restless Oyster', self.browser.title)
+
+# She opens the Graphs menu to see what kind of graphs are available
+
+# She adds a new TimePlot to the dashboard by clicking on the menu button
+
+
+if __name__ == '__main__':
+    unittest.main()
