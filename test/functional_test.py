8cdedca11b6ab695ed6cfcd2952687bb67128fbb
Tamas Gal <himself@tamasgal.com>
2014-11-12 00:28:32 +0100

    Minor adjustments

--- test/functional_test.py
+++ test/functional_test.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
 import unittest
-import selenium
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,9 +23,51 @@ class NewVisitorTest(unittest.TestCase):
         # She notices the title of the page
         self.assertIn('Restless Oyster', self.browser.title)
 
-# She opens the Graphs menu to see what kind of graphs are available
+class Graphs(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://localhost:8080"
+        self.verificationErrors = []
+        self.accept_next_alert = True
 
-# She adds a new TimePlot to the dashboard by clicking on the menu button
+    def test_create_time_plot(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_link_text("Graphs").click()
+        driver.find_element_by_link_text("TimePlot").click()
+
+
+    def test_create_histogram(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_link_text("Graphs").click()
+        driver.find_element_by_link_text("Histogram").click()
+
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException, e: return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
 
 
 if __name__ == '__main__':
