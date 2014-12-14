a3306a9b4f2874293c0c28b6a7bba843f83416f3
Tamas Gal <himself@tamasgal.com>
2014-12-14 15:32:06 +0100

    Refactorings

--- royweb/__version__.py
+++ royweb/__version__.py
@@ -1,14 +1,18 @@
-#
-# markdown/__version__.py
-#
-# version_info should conform to PEP 386 
-# (major, minor, micro, alpha/beta/rc/final, #)
-# (1, 1, 2, 'alpha', 0) => "1.1.2.dev"
-# (1, 2, 0, 'beta', 2) => "1.2b2"
+#!/usr/bin/env python
+# coding=utf-8
+# Filename: __version__.py
+"""
+Pep 386 compliant version info.
+
+    (major, minor, micro, alpha/beta/rc/final, #)
+    (1, 1, 2, 'alpha', 0) => "1.1.2.dev"
+    (1, 2, 0, 'beta', 2) => "1.2b2"
+
+"""
 version_info = (0, 8, 3, 'alpha', 0)
 
-def _get_version():
-    " Returns a PEP 386-compliant version number from version_info. "
+def _get_version(version_info):
+    """Return a PEP 386-compliant version number."""
     assert len(version_info) == 5
     assert version_info[3] in ('alpha', 'beta', 'rc', 'final')
 
@@ -17,7 +21,6 @@ def _get_version():
 
     sub = ''
     if version_info[3] == 'alpha' and version_info[4] == 0:
-        # TODO: maybe append some sort of git info here??
         sub = '.dev'
     elif version_info[3] != 'final':
         mapping = {'alpha': 'a', 'beta': 'b', 'rc': 'c'}
@@ -25,4 +28,4 @@ def _get_version():
 
     return str(main + sub)
 
-version = _get_version()
+version = _get_version(version_info)
