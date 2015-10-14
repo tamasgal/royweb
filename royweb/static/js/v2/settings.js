de4ac8f652ed42260c19d1023c6b669405072d54
Tamas Gal <himself@tamasgal.com>
2015-10-14 21:39:37 +0200

    Implements observer pattern

--- royweb/static/js/v2/settings.js
+++ royweb/static/js/v2/settings.js
@@ -2,20 +2,29 @@
     'use strict';
     var module = angular.module('ROyWeb');
 
-    module.factory('settings', function(){
+    module.factory('settings', function($rootScope){
         var factory = {};
 
         var state = 'OFF';
-
         factory.state = state;
+
+        factory.subscribe = function(scope, callback) {
+            var handler = $rootScope.$on('settings-service-event', callback);
+            scope.$on('$destroy', handler);
+        };
+
+        factory.notify = function() {
+            $rootScope.$emit('settings-service-event');
+        };
 
         return factory;
     });
 
+    // Emit signal when something in settings is changed
     module.run(function($rootScope, settings){
         $rootScope.settings = settings;
         $rootScope.$watch('settings', function(){
-            //alert('Watch');
+            settings.notify();
         }, true);
     });
 }());
