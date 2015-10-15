eff25ff9cc444f029b997bd933eddbe1de0e72bb
Tamas Gal <himself@tamasgal.com>
2015-10-15 09:50:38 +0200

    Log changes

--- royweb/static/js/v2/settings.js
+++ royweb/static/js/v2/settings.js
@@ -7,6 +7,7 @@
 
         var state = 'OFF';
         factory.state = state;
+        factory.show_point = false;
 
         factory.subscribe = function(scope, callback) {
             var handler = $rootScope.$on('settings-service-event', callback);
@@ -20,10 +21,11 @@
         return factory;
     });
 
-    // Emit signal when something in settings is changed
+    // Emit signal when something in settings has changed
     module.run(function($rootScope, settings){
         $rootScope.settings = settings;
         $rootScope.$watch('settings', function(){
+            console.log("Settings changed");
             settings.notify();
         }, true);
     });
