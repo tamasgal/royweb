e6658f9d66f6073458203f6d5a5ddd27584f4427
Tamas Gal <himself@tamasgal.com>
2015-10-16 08:44:55 +0200

    Adds MainHandler

--- royweb/static/js/v2/app.js
+++ royweb/static/js/v2/app.js
@@ -1,3 +1,14 @@
 (function () {
     angular.module('ROyWeb', ['ngTouch', 'gridshore.c3js.chart']);
- }());
+
+    'use strict';
+    var module = angular.module('ROyWeb');
+
+    module.controller('MainController',
+            ['$scope', 'settings', function($scope, settings) {
+
+        $scope.reset_graph_selection = function() {
+            settings.selected_graph = false;
+        };
+    }]);
+}());
