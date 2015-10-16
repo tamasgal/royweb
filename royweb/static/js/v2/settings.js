645887051949b8a1109e3b5c3b9b99517fbce60a
Tamas Gal <himself@tamasgal.com>
2015-10-16 08:47:15 +0200

    Changes general event type to more specific event emission

--- royweb/static/js/v2/settings.js
+++ royweb/static/js/v2/settings.js
@@ -7,15 +7,17 @@
 
         var state = 'OFF';
         factory.state = state;
+        factory.selected_graph = false;
         factory.show_point = false;
 
-        factory.subscribe = function(scope, callback) {
-            var handler = $rootScope.$on('settings-service-event', callback);
+        factory.subscribe = function(scope, event, callback) {
+            var handler = $rootScope.$on(event, callback);
             scope.$on('$destroy', handler);
         };
 
-        factory.notify = function() {
-            $rootScope.$emit('settings-service-event');
+        factory.notify = function(event) {
+            console.log("Emmiting " + event);
+            $rootScope.$emit(event);
         };
 
         return factory;
@@ -24,9 +26,9 @@
     // Emit signal when something in settings has changed
     module.run(function($rootScope, settings){
         $rootScope.settings = settings;
-        $rootScope.$watch('settings', function(){
+        $rootScope.$watch('settings.selected_graph', function(){
             console.log("Settings changed");
-            settings.notify();
+            settings.notify('selected-graph-event');
         }, true);
     });
 }());
